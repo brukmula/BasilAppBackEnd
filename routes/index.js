435554d3cc72cbd28f81e46bf6628bccb8b219e2
Samuel Howard <samuelhoward0725@gmail.com>
2023-07-03 13:30:31 -0400

    Made a basic API reflection
    Signed-off-by: Samuel Howard <samuelhoward0725@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,50 @@
-var express = require('express');
-var router = express.Router();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const compression = require('compression')
+const express = require('express');
+const helmet = require('helmet');
+// const morgan = require('morgan');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const app = express();
+const port = process.env.PORT || 3000;
+
+async function net_bible(book, chapter) {
+  try {
+    const response = await fetch(`https://labs.bible.org/api/?passage=${book}%20${chapter}&type=json`);
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
+
+// adding Helmet the APIs security
+app.use(helmet());
+
+// using bodyParser to parse JSON bodies into JS objects
+app.use(bodyParser.json());
+
+// enabling CORS for all requests
+app.use(cors());
+
+// Use compression to ease network burden
+app.use(compression())
+
+// TODO: Use later // adding morgan to log HTTP requests
+// app.use(morgan('combined'));
+
+// http://127.0.0.1:3000/api/bible/?book=john&chapter=3
+app.get('/api/bible', (req, res) => {
+  let book = req.query.book;
+  console.log(book);
+  let chapter = req.query.chapter;
+  console.log(chapter);
+  net_bible(book, chapter).then( (data) => res.send(data) );
 });
 
-module.exports = router;
+// starting the server
+app.listen(port, () => {
+  console.log(`API server listening on port ${port}`);
+});
+
+module.exports = app;
