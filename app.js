53fcd4cb5ddeaa8290e4ddc539863bc80f7e5578
Samuel Howard <sahoward42@tntech.edu>
2023-07-27 13:49:00 -0400

    Removed error rendering (this is an API, not a front end)
    Signed-off-by: Samuel Howard <sahoward42@tntech.edu>

--- app.js
+++ app.js
@@ -59,22 +59,6 @@ app.get('/health', (req, res) => {
   res.status(200).send("Healthy: OK");
 })
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 app.listen(port, () => {
   console.log(`API server listening on port ${port}`);
 });
